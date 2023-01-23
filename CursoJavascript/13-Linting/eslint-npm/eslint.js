fcffb867e44535a1cda79880502530363ebff137
Edgar Avila Gonzalez <avilaedgar2001@gmail.com>
2023-01-23 10:03:56 -0600

    bloque 13 curso javascript openbootcamp terminado

---
+++ CursoJavascript/13-Linting/eslint-npm/eslint.js
@@ -0,0 +1,25 @@
+//Eslint mediante descarga
+var hola = "edga";
+console.log(hola);
+
+var varr = "cola";
+console.log(varr);
+
+//Desactivar eslint de todo
+/*eslint-disable */
+var desactivado = 'true' //no pide cambiar las comillas porque eslint esta desactivado
+
+//Activar de nuevo
+/*eslint-enable */
+//aque si pide corregir porque ya esta activado de nuevo eslint //Desactivar por linea
+var activado = 'true' /*eslint-disable-line*/
+
+//Desactivar siguiente linea
+/*eslint-disable-next-line */
+    const string = 'hola'
+
+ console.log("hola")
+
+  const hola = 10
+  const hola = 1
+  
---
+++ CursoJavascript/13-Linting/index.js
@@ -0,0 +1,8 @@
+const nombre = "Edgar";
+
+    
+
+    var nombre2 = 'Jaime';
+
+    var nombre2 = 'javir';
+    
